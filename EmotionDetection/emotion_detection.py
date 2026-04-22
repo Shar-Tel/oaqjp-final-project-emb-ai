ebb4c0fee9d49ffa1b1a4e026981e9af7d2bd8cc
Shar-Tel <118901306+Shar-Tel@users.noreply.github.com>
2026-04-22 12:34:04 +0200

    Implement emotion detection function using API
    This function analyzes text input to detect emotions using a specified API. It returns scores for various emotions and identifies the dominant emotion based on the highest score.

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,70 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    # Define the URL for the emotion detection API
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create the payload with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyze } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json=myobj, headers=header)
+
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        dominant_emotion="anger"
+        higher_score=float(anger_score)
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        if float(disgust_score)>higher_score:
+            dominant_emotion="disgust"
+            higher_score=float(disgust_score)        
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        if float(fear_score)>higher_score:
+            dominant_emotion="fear"
+            higher_score=float(fear_score)  
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        if float(joy_score)>higher_score:
+            dominant_emotion="joy"
+            higher_score=float(joy_score)  
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        if float(sadness_score)>higher_score:
+            dominant_emotion="sadness"
+            higher_score=float(sadness_score)  
+    # If the response status code is 400, set all to None
+    elif response.status_code == 400:
+        return {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+        }
+
+    # For any other unexpected status codes, set all to None
+    else:
+        return {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+        }
+
+    # # Return the label and score in a dictionary
+    return {
+    'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': dominant_emotion
+    }
